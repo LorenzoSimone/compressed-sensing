ad022ca56cfc8542dca28c1a559b1bad49f3ca27
LorenzoSimone <loughl@hotmail.it>
2020-11-22 12:42:24 +0100

    Fix revised dual simplex

--- MatLab/dualsimplex.m
+++ MatLab/dualsimplex.m
@@ -1,19 +1,22 @@
-function dualsimplex(A,b,c,state)
+function dualsimplex(A,b,c,B,state)
 
-%Finding a good basis
 [m, n] = size(A);
 ROWS = linspace(1,m,m);
-B = ROWS(1:n);
-N = ROWS(n+1:m);
 iteration  = 1;
+
+%Default basis if it is not provided
+if ~length(B)  
+    B = ROWS(1:n);  
+    disp('non me lo dai');
+end
+
+N = setdiff(ROWS, B);
 
 while(state == "")
     
     fprintf('||--------Iteration [%d]--------||\n', iteration);
     A_b = A(B,:); A_N = A(N,:);
     b_b = b(B); b_N = b(N);
-    B
-    A_b
     A_b_inv = inv(A_b);
 
     %Initialization of x_ and y_
@@ -41,25 +44,25 @@ while(state == "")
     if all(eta_b <= zeros(size(eta_b)))
         fprintf('||--------End [%d]--------||\n', iteration);
         fprintf('||--------Primal set is empty--------||\n');
+        %[DEBUG] Exercise 2.33 Printing variables 
+        A_nx
+        b_N
+        eta_b
+        c*x
+        k
         state = "P.empty";
         break
     end
     
     %Finding the exiting variable
-    pos_eta = find(eta_b > 0);
-    y_b = y(B);
-    
-    [theta, h] = min(y_b(pos_eta)./(eta_b(pos_eta)));
-    k
-    h = B(h);
+    theta = y(B)./eta_b;
 
-    %Recalculating Basic and Non-Basic index vectors
-    B = union(setdiff(B, h), k);
-    N = setdiff(ROWS, B);
-    iteration = iteration+1;
+    [v, i] = min(theta(eta_b>0));
+    i = find(theta == v & eta_b > 0);
+    h = B(i);
     
     %[DEBUG] Printing variables as in the order of Exercise 2.31
-    %{
+    B
     x
     A_nx
     b_N
@@ -67,6 +70,9 @@ while(state == "")
     k
     eta_b
     h
-    %}
     
+    %Recalculating Basic and Non-Basic index vectors
+    B(B==h)=k;
+    N = setdiff(ROWS, B);
+    iteration = iteration+1;
 end
